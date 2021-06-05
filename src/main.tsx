e305e1c9274ba26605f9949af13a62527bc17fdd
Peng Xiao <pengxiao@outlook.com>
2021-06-05 22:19:27 +0800

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,61 @@
+import "@logseq/libs";
+import "virtual:windi.css";
+
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+// @ts-expect-error
+const css = (t, ...args) => String.raw(t, ...args);
+
+function main() {
+  const pluginId = logseq.baseInfo.id;
+  console.info(`#${pluginId}: MAIN`);
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("app")
+  );
+
+  function createModel() {
+    return {
+      show() {
+        logseq.showMainUI();
+      },
+    };
+  }
+
+  logseq.provideModel(createModel());
+  logseq.setMainUIInlineStyle({
+    zIndex: 11,
+  });
+
+  const openIconName = "template-plugin-open";
+
+  logseq.provideStyle(css`
+    div[data-injected-ui=${openIconName}-${pluginId}] {
+      display: inline-flex;
+      align-items: center;
+      opacity: 0.55;
+      font-weight: 500;
+      padding: 0 5px;
+      position: relative;
+    }
+
+    div[data-injected-ui=${openIconName}-${pluginId}]:hover {
+      opacity: 0.9;
+    }
+  `);
+
+  logseq.provideUI({
+    key: openIconName,
+    path: "#search",
+    template: `
+      <a data-on-click="show"
+         style="opacity: .6; display: inline-flex;">⚙️</a>
+    `,
+  });
+}
+
+logseq.ready(main).catch(console.error);
