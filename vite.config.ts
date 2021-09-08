b6fbbd00f812481f1a0d7441914f02f8417be34e
Peng Xiao <pengxiao@freewheel.tv>
2021-09-08 15:39:19 +0800

    fix: revert logseq libs back to 26

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig, Plugin, ResolvedConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 import WindiCSS from "vite-plugin-windicss";
-// @ts-expect-error
 import { writeFile, mkdir } from "fs/promises";
 import path from "path";
 
